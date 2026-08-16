bf561599048a6d5ae568bfd2a8f6a894dd414235
norihitoishida <norihitoishida@gmail.com>
2026-08-17 04:13:25 +0900

    Dupdate

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,6 +8,7 @@ TIMEZONE = 'Asia/Tokyo'
 
 DEFAULT_LANG = 'ja'
 
+##########
 FAVICON = 'favicon.ico'
 FAVICON_TYPE = 'image/vnd.microsoft.icon'
 
@@ -15,6 +16,7 @@ STATIC_PATHS = ['images', 'extra']
 EXTRA_PATH_METADATA = {
     'extra/' + FAVICON: {'path': FAVICON},
 }
+##########
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -23,21 +25,17 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
+# Link
 LINKS = [
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-]
-
-# Social widget
-SOCIAL = [
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("YouTube", "https://youtube.com/channel/UCNm0hb-n5Apt6GWajJ6S8Og/?sub_confirmation=1"),
+    ("Twitter", "https://x.com/TCP_Hz"),
+    ("LINK3", "https://getpelican.com/"),
+    ("LINK4", "https://getpelican.com/"),
 ]
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+##########
