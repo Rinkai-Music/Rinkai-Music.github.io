472a299dacff6823cd0cc2d953f9d0b4eb008da5
norihitoishida <norihitoishida@gmail.com>
2026-08-22 19:13:35 +0900

    update

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+AUTHOR = "RINKAI"
+SITENAME = "RINKAI's Homepage"
+TIMEZONE = 'Asia/Tokyo'
+DEFAULT_LANG = 'ja'
+
+PATH = "content"
+
+##########
+FAVICON = 'favicon.ico'
+FAVICON_TYPE = 'image/vnd.microsoft.icon'
+STATIC_PATHS = ['images', 'extra/favicon.ico']
+EXTRA_PATH_METADATA = {'extra/favicon.ico': {'path': 'favicon.ico'},}
+
+##########
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Link
+LINKS = [
+    ("YouTube", "https://youtube.com/channel/UCNm0hb-n5Apt6GWajJ6S8Og/?sub_confirmation=1"),
+    ("Twitter", "https://x.com/TCP_Hz"),
+    ("GitHub", "https://github.com/Rinkai-Music/"),
+]
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+##########
