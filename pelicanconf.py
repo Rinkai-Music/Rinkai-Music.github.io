9a1b3d791c8808938b382ccb97959dbc55b93253
norihitoishida <norihitoishida@gmail.com>
2026-08-22 18:37:06 +0900

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,10 +12,8 @@ DEFAULT_LANG = 'ja'
 FAVICON = 'favicon.ico'
 FAVICON_TYPE = 'image/vnd.microsoft.icon'
 
-STATIC_PATHS = ['images', 'extra']
-EXTRA_PATH_METADATA = {
-    'extra/' + FAVICON: {'path': FAVICON},
-}
+STATIC_PATHS = ['images', 'favicon.ico']
+
 ##########
 
 # Feed generation is usually not desired when developing
@@ -29,8 +27,7 @@ AUTHOR_FEED_RSS = None
 LINKS = [
     ("YouTube", "https://youtube.com/channel/UCNm0hb-n5Apt6GWajJ6S8Og/?sub_confirmation=1"),
     ("Twitter", "https://x.com/TCP_Hz"),
-    ("LINK3", "https://getpelican.com/"),
-    ("LINK4", "https://getpelican.com/"),
+    ("GitHub", "https://github.com/Rinkai-Music/"),
 ]
 
 DEFAULT_PAGINATION = 10
